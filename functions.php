54087d07f460bdf61f1c798bd43f8a0fb4c4d25f
everaldomatias <matiaseveraldo@gmail.com>
2022-05-04 22:27:59 -0300

    Add gitignore and custom CSS

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ function betheme_parent_theme_enqueue_styles() {
 	wp_enqueue_style( 'betheme-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'pontodaprata-style',
 		$get_stylesheet_directory_uri . '/style.css',
-		[ 'betheme-style' ],
+		[ 'betheme-style', 'mfn-be', 'mfn-woo' ],
 		$rand
 	);
 
